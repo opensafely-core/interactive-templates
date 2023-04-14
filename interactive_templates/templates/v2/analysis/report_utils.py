15d7e50214c7f473ae355041e977a20287f39270
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2023-04-14 15:39:33 +0100

    update `generate_expectations_codes` This now properly handles codelists that have less than 10 codes

--- interactive_templates/templates/v2/analysis/report_utils.py
+++ interactive_templates/templates/v2/analysis/report_utils.py
@@ -46,8 +46,11 @@ def relabel_sex(df):
 
 
 def generate_expectations_codes(codelist, incidence=0.5):
-    expectations = {str(x): (1 - incidence) / 10 for x in codelist[0:10]}
-    # expectations = {str(x): (1-incidence) / len(codelist) for x in codelist}
+    if len(codelist) >= 10:
+        expectations = {str(x): (1 - incidence) / 10 for x in codelist[0:10]}
+    else:
+        expectations = {str(x): (1 - incidence) / len(codelist) for x in codelist}
+
     expectations[None] = incidence
     return expectations
 
