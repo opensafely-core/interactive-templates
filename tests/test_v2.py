68ae6f6cf8ae3878db68ce572ca52f5c5a3815b9
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2023-04-26 14:50:42 +0100

    assert feather file in functional tests

--- tests/test_v2.py
+++ tests/test_v2.py
@@ -35,5 +35,5 @@ def test_v2_functional_time_ever(tmp_path):
     # speed things up by only testing the base study definition
     assert local_run.main(tmp_path, ["generate_study_population_weekly_id"])
     assert (
-        tmp_path / f"output/id/input_weekly_{ analysis.week_of_latest_extract }.csv.gz"
+        tmp_path / f"output/id/input_weekly_{ analysis.week_of_latest_extract }.feather"
     ).exists()
