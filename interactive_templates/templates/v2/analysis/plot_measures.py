22280e5baf12dafd4b4582f140ef877e16bfe986
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2023-05-04 15:06:21 +0100

    only plot what there is data for
    Signed-off-by: Louis Fisher <louis.fisher1919@gmail.com>

--- interactive_templates/templates/v2/analysis/plot_measures.py
+++ interactive_templates/templates/v2/analysis/plot_measures.py
@@ -6,9 +6,6 @@ from report_utils import deciles_chart, plot_measures
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--breakdowns", action="append", default=[], help="breakdowns to use"
-    )
     parser.add_argument("--output-dir", help="output directory", required=True)
     args = parser.parse_args()
     return args
@@ -16,22 +13,30 @@ def parse_args():
 
 def main():
     args = parse_args()
-    breakdowns = args.breakdowns
 
     df = pd.read_csv(
         f"{ args.output_dir }/joined/measure_all.csv", parse_dates=["date"]
     )
+    breakdowns = df["group"].unique().tolist()
+    breakdowns = [
+        b
+        for b in breakdowns
+        if b not in ("total", "practice", "event_1_code", "event_2_code")
+    ]
+
     df = df.loc[df["value"] != "[Redacted]", :]
     df["value"] = df["value"].astype(float)
 
     df_total = df.loc[df["group"] == "total", :]
-    plot_measures(
-        df_total,
-        filename=f"{ args.output_dir }/plot_measures",
-        column_to_plot="value",
-        y_label="Rate per 1000",
-        category=None,
-    )
+
+    if not df_total.empty:
+        plot_measures(
+            df_total,
+            filename=f"{ args.output_dir }/plot_measures",
+            column_to_plot="value",
+            y_label="Rate per 1000",
+            category=None,
+        )
 
     for breakdown in breakdowns:
         df_subset = df.loc[df["group"] == breakdown, :]
